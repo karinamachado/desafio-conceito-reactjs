1ed8a49ebacea2dd23fca4d0499eb2244f889651
karinamachado <karinamachado@msn.com>
2020-11-04 15:35:07 -0300

    update desafio03

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+    // Salvar os dados do repositório
+    const [repositories, setRepositories] = useState([]);
+    // funcao para buscar os dados na api
+    useEffect(() => {
+      api.get('repositories').then(response => {
+        setRepositories(response.data);
+      });
+    },[]);
+
+
   async function handleAddRepository() {
-    // TODO
+   const response = await api.post('repositories',{
+    title: '',
+    url: '',
+    techs:[]
+   });
+
+   setRepositories([...repositories, response.data]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+       {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+       ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
